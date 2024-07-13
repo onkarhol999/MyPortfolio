21bef47b12c564d26ab0ba5f80bfe5c5f47b4eb3
Onkar hol <holonkar2003@gmail.com>
2024-07-13 19:17:06 +0530

    complete the all section only peoject section remain

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
         let screenPosition ;
         
 
-        if(window.innerWidth > 768){
+        if(window.innerWidth < 768){
             screenScale = [1.5,1.5,1.5];
             screenPosition = [0, -1.5, 0];
         }else{
@@ -72,8 +72,8 @@ const Home = () => {
            />
            <Plane
            isRotating={isRotating}
-            planeScale={planeScale}
-            planePosition={planePosition}
+            scale={planeScale}
+            position={planePosition}
             rotation={[0,20,0]}
            />
         </Suspense>
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { projects } from '../constants';
 
 const Project = () => {
   return (
-    <div>
-      Projects
-    </div>
-  )
+    <section className='max-container'> 
+      <h1 className='head-text'>
+        My <span className='blue-gradient_text font-semibold drop-shadow'>Projects</span>
+      </h1>
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          I've been involved in a diverse array of projects over the years, each contributing uniquely to my experience and growth. I invite you to explore these endeavors, each a testament to my commitment to excellence and innovation. Your interest and insights are always welcome as we strive for continued success together.
+        </p>
+      </div>
+      <div className='flex flex-wrap my-20 gap-16'>
+        {projects.map((project) => (
+          <div className='lg:w-[400px] w-full' key={project.name}>
+            <div className='block-container w-12 h-12'>
+              <div className={`btn-black rounded-xl ${project.theme}`} />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img 
+                  src={project.iconUrl}
+                  alt="Project Icon"
+                  className='w-1/2 h-1/2 object-contain' 
+                />
+              </div>
+            </div>
+            <div className='mt-5'>
+              <h3 className='subhead-text'>{project.name}</h3>
+              <p className='text-slate-500'>{project.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Project
+export default Project;
